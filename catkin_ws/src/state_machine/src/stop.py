3b8125ffaa4f4a96ae83d7177c1464cfa1adadd6
Katy Muhlrad <kmuhlrad@gmail.com>
2016-11-11 11:28:15 -0500

    added stop state

---
+++ catkin_ws/src/state_machine/src/stop.py
@@ -0,0 +1,21 @@
+from state import State
+
+class Start(State):
+    def __init__(self, current_input):
+        self.current_input = current_input
+        self.can_start = False
+    
+    def run(self):
+        self.can_start = True
+    
+    def next_input(self):
+        return 0
+
+    def next_state(self):
+        return Stop
+
+    def is_finished(self):
+        return self.can_start
+
+    def is_stop_state(self):
+        return True
