d5ad269194245ec79fcfb5c9b64c929753730387
Katy Muhlrad <kmuhlrad@gmail.com>
2016-11-29 01:41:25 -0500

    updated start state to start the match

--- catkin_ws/src/state_machine/src/start.py
+++ catkin_ws/src/state_machine/src/start.py
@@ -1,6 +1,5 @@
 from state import State
 from search import Search
-from catch import Catch
 
 class Start(State):
     def __init__(self, current_input):
@@ -8,15 +7,13 @@ class Start(State):
         self.can_start = False
     
     def run(self):
-        # wait for start signal - modify canStart
         self.can_start = True
     
     def next_input(self):
-        return 2.5 # for actual thing
-        #return 6 # for testing drive
+        return 2.5
 
     def next_state(self):
-        return Catch(self.next_input())
+        return Search(self.next_input())
 
     def is_finished(self):
         return self.can_start
